dcd748dff32d95152e1350da6ac72f8b1c3f97ec
Pawel Gorniak <gorniak.p@gmail.com>
2017-11-14 05:44:33 +0100

    file

--- jsonp.js
+++ jsonp.js
@@ -1,7 +1,7 @@
-buses() {
+buses({
         "events": [
             {"location": "San Franisco", "date": "1 May", "map": "map/map.jpg"},
             {"location": "New York", "date": "15 May", "map": "map/map.jpg"},
             {"location": "Ohio", "date": "10 May", "map": "map/map.jpg"}
         ]
-    }
+    });
